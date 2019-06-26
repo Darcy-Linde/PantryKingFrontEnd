6a7e858f897b0a1d64674fb0814dd62db60f1661
Darcy Linde <lindedarcy@gmail.com>
2019-06-26 10:53:04 -0400

    opening localhost now redirects to login page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import LoginForm from "./components/LoginForm";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Switch>
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={LoginForm} />
         <Route exact path="/signup" component={SignUp} />
